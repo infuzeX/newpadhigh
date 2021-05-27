105d965453880a28998d768987fdfa2dd9eefadf
abhi <abhikp2000@gmail.com>
2021-05-28 00:03:07 +0530

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  
+ 
+    $('.toggle').on('click', function() {
+          // this toggle is from jQueryUI
+          $('.block-two').toggle('slide', 500);
+      
+      
+    });
+  
+  
+  });
